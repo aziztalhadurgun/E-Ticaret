1401453dbb51c5485c22b8e70c733ca23daa01ad
Aziz Talha <aziztalhadurgun@gmail.com>
2018-04-25 23:40:37 +0300

    Update Iletisim.cs

--- E-Ticaret/Models/Iletisim.cs
+++ E-Ticaret/Models/Iletisim.cs
@@ -8,7 +8,7 @@ namespace E_Ticaret.Models
     public class Iletisim
     {
         public int Id { get; set; }
-        public string Ad { get; set; }
+        public int Ad { get; set; }
         public string Soyad { get; set; }
         public string Email { get; set; }
         public string Konu { get; set; }
